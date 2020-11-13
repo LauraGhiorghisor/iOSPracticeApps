eae63a808e0512917a22f8b61efdd4805d34795e
Laura Ghiorghisor <laura.ghiorghisor@gmail.com>
2020-11-13 23:46:11 +0000

    Add all apps

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Laura Ghiorghisor on 07/06/2020.
+//  Copyright © 2020 Laura Ghiorghisor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
